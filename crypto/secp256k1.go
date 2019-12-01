98ed052f232accd12915cf7223782a17ca58dc9e
Gregory Markou <gregorymarkou@gmail.com>
2019-12-01 11:13:30 -0500

    additional methods

--- crypto/secp256k1.go
+++ crypto/secp256k1.go
@@ -6,27 +6,6 @@ import (
 	secp "github.com/ethereum/go-ethereum/crypto"
 )
 
-// type Keypair interface {
-// 	Sign(msg []byte) ([]byte, error)
-// 	Public() PublicKey
-// 	Private() PrivateKey
-// }
-
-// type PublicKey interface {
-// 	Verify(msg, sig []byte) bool
-// 	Encode() []byte
-// 	Decode([]byte) error
-// 	Address() common.Address
-// 	Hex() string
-// }
-
-// type PrivateKey interface {
-// 	Sign(msg []byte) ([]byte, error)
-// 	Public() (PublicKey, error)
-// 	Encode() []byte
-// 	Decode([]byte) error
-// }
-
 type SecpKeypair struct {
 	public  *SecpPublicKey
 	private *SecpPrivateKey
@@ -35,20 +14,25 @@ type SecpKeypair struct {
 type SecpPublicKey secp.PublicKey
 type SecpPrivateKey secp.PrivateKey
 
-func NewSecpKeypair(pk SecpPrivateKey) *SecpKeypair {
-	pubkey := secp.PublicKey
-}
+func NewSecpKeypair(pk SecpPrivateKey) *SecpKeypair {}
 
 // Keypair methods
 func (kp *SecpKeypair) Sign(msg []byte) ([]byte, error) {
 	return kp.private.Sign(msg)
 }
 
-func (kp *SecpKeypair) Public() keys.PublicKey {}
-func (kp *SecpKeypair) Private() keys.PrivateKey {}
+func (kp *SecpKeypair) Public() keys.PublicKey {
+	return kp.public
+}
+func (kp *SecpKeypair) Private() keys.PrivateKey {
+	return kp.private
+}
 
 // PublicKey methods
-func (k *SecpPublicKey) Verify(msg, sig []byte) bool {}
+func (k *SecpPublicKey) Verify(msg, sig []byte) bool {
+	return secp.VerifySignature(k, msg, sig)
+}
+
 func (k *SecpPublicKey) Encode() []byte {}
 func (k *SecpPublicKey) Decode([]byte) error {}
 func (k *SecpPublicKey) Address() common.Address {}
@@ -59,8 +43,10 @@ func (pk *SecpPrivateKey) Sign(msg []byte) ([]byte, error) {
 	return secp.Sign(msg, pk.private)
 }
 
-func (pk *SecpPrivateKey) Public() (PublicKey, error) {}
-func (pk *SecpPrivateKey) Sign(msg []byte) ([]byte, error) {}
-func (pk *SecpPrivateKey) Public() (PublicKey, error) {}
+func (pk *SecpPrivateKey) Public() (PublicKey, error) {
+	kp := NewSecpKeypair(pk)
+	return kp.private, nil
+}
+
 func (pk *SecpPrivateKey) Encode() []byte {}
 func (pk *SecpPrivateKey) Decode([]byte) error {}
