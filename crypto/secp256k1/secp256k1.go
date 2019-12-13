c41eab9ae5fb2d565fee6f901a3357dbdae2b332
noot <36753753+noot@users.noreply.github.com>
2019-12-13 15:38:19 -0500

    ethereum sign tx (#39)
    * add keypair to eth connection
    * implement signing of tx in SubmitTx for ethereum
    * to sign a tx, the function takes a Signer which is created from a geth ChainConfig. we need to add which eth chain config to use to the config, see todo in chains/ethereum/ethereum.go

--- crypto/secp256k1/secp256k1.go
+++ crypto/secp256k1/secp256k1.go
@@ -55,6 +55,7 @@ func (kp *Keypair) Sign(msg []byte) ([]byte, error) {
 func (kp *Keypair) Public() crypto.PublicKey {
 	return kp.public
 }
+
 func (kp *Keypair) Private() crypto.PrivateKey {
 	return kp.private
 }
@@ -115,4 +116,8 @@ func (pk *PrivateKey) Decode(in []byte) error {
 	key := secp256k1.ToECDSAUnsafe(in)
 	pk.key = *key
 	return nil
+}
+
+func (pk *PrivateKey) Key() *ecdsa.PrivateKey {
+	return &pk.key
 }
