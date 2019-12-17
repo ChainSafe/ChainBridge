2ca121b68317c400a2f903f35577a60bb4181b6f
David Ansermino <david@chainsafe.io>
2019-12-17 09:13:55 -0500

    Adds tests, locks

--- core/router/router.go
+++ core/router/router.go
@@ -2,32 +2,43 @@ package router
 
 import (
 	"fmt"
+	"sync"
 
 	msg "github.com/ChainSafe/ChainBridgeV2/message"
 	log "github.com/ChainSafe/log15"
 )
 
+// Router forwards messages from their source to their destination
 type Router struct {
 	registry map[msg.ChainId]chan<- msg.Message
+	lock sync.RWMutex
 }
 
 func NewRouter() *Router {
-	return &Router{registry: make(map[msg.ChainId]chan<- msg.Message)}
+	return &Router{
+		registry: make(map[msg.ChainId]chan<- msg.Message),
+		lock: sync.RWMutex{},
+	}
 }
 
 // Send passes a message to the destination channel if it exists
 func (r *Router) Send(msg msg.Message) error {
+	r.lock.RLock()
+	defer r.lock.RUnlock()
+	log.Trace("Sending message", "src", msg.Source.String(), "dest", msg.Destination.String())
 	ch := r.registry[msg.Destination]
 	if ch == nil {
 		return fmt.Errorf("unknown destination chainId: %d", msg.Destination)
 	}
-	// TODO: This will block, we want to avoid this but preserve ordering, perhaps a queue would help
-	ch <- msg
+	// TODO: Need to preserve ordering, perhaps a queue would help
+	go func (){ch <- msg}()
 	return nil
 }
 
 // Listen registers a channel with a ChainId which Send can then use to forward messages
 func (r *Router) Listen(id msg.ChainId, ch chan<- msg.Message) error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
 	log.Debug("Registering new chain in router", "id", id)
 	if r.registry[id] != nil {
 		return fmt.Errorf("attempted to register chain that is already registered")
--- core/router/router_test.go
+++ core/router/router_test.go
@@ -1,9 +1,51 @@
 package router
 
 import (
+	"reflect"
 	"testing"
+
+	msg "github.com/ChainSafe/ChainBridgeV2/message"
 )
 
 func TestRouter(t *testing.T) {
+	router := NewRouter()
 
+	ethCh := make(chan msg.Message)
+	err := router.Listen(msg.EthereumId, ethCh)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctfgCh := make(chan msg.Message)
+	err = router.Listen(msg.CentrifugeId, ctfgCh)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgEthToCtfg := msg.Message{
+		Source:      msg.EthereumId,
+		Destination: msg.CentrifugeId,
+	}
+
+	msgCtfgToEth := msg.Message{
+		Source:      msg.CentrifugeId,
+		Destination: msg.EthereumId,
+	}
+
+	err = router.Send(msgCtfgToEth)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = router.Send(msgEthToCtfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(<- ethCh, msgCtfgToEth) {
+		t.Error("Unexpected message")
+	}
+
+	if !reflect.DeepEqual(<- ctfgCh, msgEthToCtfg) {
+		t.Error("Unexpected message")
+	}
 }
