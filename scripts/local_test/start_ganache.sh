b7a9cd64d52c686a57426ce0a23c9162e645f9a1
Gregory Markou <gregorymarkou@gmail.com>
2019-12-19 18:49:42 -0500

    ci

--- scripts/local_test/start_ganache.sh
+++ scripts/local_test/start_ganache.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 pushd ./contracts/evm-contracts &&
-npm install &&
-node_modules/.bin/ganache-cli -m when sound uniform light fee face forum huge impact talent exhaust arrow
+npm install
+
+if [ $1 = "silent" ]; then
+    node_modules/.bin/ganache-cli -m when sound uniform light fee face forum huge impact talent exhaust arrow > /dev/null &
+else 
+    node_modules/.bin/ganache-cli -m when sound uniform light fee face forum huge impact talent exhaust arrow
+fi
