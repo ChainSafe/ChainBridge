059d18dedf14dc7ce062806a09d1b5ffdf509c31
noot <elizabethjbinks@gmail.com>
2019-12-19 11:32:28 -0500

    call centrifuge store function; add helpers

--- chains/ethereum/writer_test.go
+++ chains/ethereum/writer_test.go
@@ -6,15 +6,17 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/ChainSafe/ChainBridgeV2/common"
 	"github.com/ChainSafe/ChainBridgeV2/crypto/secp256k1"
 	msg "github.com/ChainSafe/ChainBridgeV2/message"
-	//ethcommon "github.com/ethereum/go-ethereum/common"
+	ethcommon "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	ethparams "github.com/ethereum/go-ethereum/params"
 )
 
 var TestPrivateKey = "ae6a8b4518e3970a0501ecf796a51dc0dab9143a66be75e948bf352582db15d5"
-var TestPublicKey = "C5F737aE7EaBB7226f21121E335b0949d8eA6365"
+var TestAddress = "C5F737aE7EaBB7226f21121E335b0949d8eA6365"
+var TestContractAddress = "f563197e48e62ae4ce194e89f7fca67e05cfe5fc"
 
 func testHandler(t *testing.T) msg.Message {
 	// arbitrary hash
@@ -29,11 +31,9 @@ func testHandler(t *testing.T) msg.Message {
 	}
 }
 
-// func TestResolveMessage(t *testing.T) {
-// 	m := testHandler(t)
-// }
+func TestResolveMessage(t *testing.T) {
+	m := testHandler(t)
 
-func TestWriteToCentrifugeContract(t *testing.T) {
 	ctx := context.Background()
 
 	signer := ethtypes.MakeSigner(ethparams.MainnetChainConfig, big.NewInt(0))
@@ -66,40 +66,91 @@ func TestWriteToCentrifugeContract(t *testing.T) {
 	}
 	defer conn.Close()
 
-	// addressBytes, err := hex.DecodeString(TestPublicKey)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	w := NewWriter(conn)
+	err = w.ResolveMessage(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
 
-	// address := [20]byte{}
-	// copy(address[:], addressBytes)
+func TestWriteToCentrifugeContract(t *testing.T) {
+	ctx := context.Background()
 
-	// currBlock, err := conn.LatestBlock()
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	signer := ethtypes.MakeSigner(ethparams.MainnetChainConfig, big.NewInt(0))
+	privBytes, err := hex.DecodeString(TestPrivateKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	priv, err := secp256k1.NewPrivateKey(privBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// nonce, err := conn.NonceAt(address, currBlock.Number())
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	kp, err := secp256k1.NewKeypairFromPrivate(priv)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// tx := ethtypes.NewTransaction(
-	// 	nonce,
-	// 	ethcommon.Address(address),
-	// 	big.NewInt(0),
-	// 	1000000, // gasLimit
-	// 	big.NewInt(10), // gasPrice
-	// 	[]byte{},
-	// )
+	// TODO: add run ./scripts/local_test/start_ganache.sh and ./scripts/local_test/ethereum_start.sh
+	// to CI so that contract gets deployed
+	cfg := &ConnectionConfig{
+		Ctx:      ctx,
+		Endpoint: "http://localhost:8545",
+		Keypair:  kp,
+		Signer:   signer,
+		HttpConn: true,
+		Away:     TestContractAddress,
+	}
 
-	// data, err := tx.MarshalJSON()
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	conn := NewConnection(cfg)
+	err = conn.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
 
-	// err = conn.SubmitTx(data)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	contractBytes, err := hex.DecodeString("F60D9c8AC3B9B88483cee749b25117330F927780")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	contract := [20]byte{}
+	copy(contract[:], contractBytes)
+
+	currBlock, err := conn.LatestBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nonce, err := conn.NonceAt(common.StringToAddress(TestAddress), currBlock.Number())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	calldata := common.FunctionId("store(bytes32)")
+	// arbitrary hash
+	hash, err := hex.DecodeString("58c4b8bfc2aa5dd1da2f02da071c35c1c9f1bc95c4b8b957d44119af6abd5df9")
+	if err != nil {
+		t.Fatal(err)
+	}
+	calldata = append(calldata, hash...)
+
+	tx := ethtypes.NewTransaction(
+		nonce,
+		ethcommon.Address(contract),
+		big.NewInt(0),
+		1000000,        // gasLimit
+		big.NewInt(10), // gasPrice
+		calldata,
+	)
+
+	data, err := tx.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = conn.SubmitTx(data)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
---
+++ common/helpers.go
@@ -0,0 +1,43 @@
+package common
+
+import (
+	"encoding/hex"
+	"fmt"
+	"syscall"
+
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// GetPassword displays a message and prompts user to enter a password
+func GetPassword(msg string) []byte {
+	for {
+		fmt.Println(msg)
+		fmt.Print("> ")
+		password, err := terminal.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Printf("invalid input: %s\n", err)
+		} else {
+			fmt.Printf("\n")
+			return password
+		}
+	}
+}
+
+// StringToAddress turns an Ethereum address in string form into a byte array
+func StringToAddress(addr string) [20]byte {
+	addrBytes, err := hex.DecodeString(addr)
+	if err != nil {
+		return [20]byte{}
+	}
+	addrArr := [20]byte{}
+	copy(addrArr[:], addrBytes)
+	return addrArr
+}
+
+// FunctionId returns a 4-byte function ID given the signature
+func FunctionId(sig string) []byte {
+	bytes := []byte(sig)
+	hash := ethcrypto.Keccak256(bytes)
+	return hash[0:4]
+}
