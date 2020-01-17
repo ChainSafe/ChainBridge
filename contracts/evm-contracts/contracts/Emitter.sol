1eae9b8e8210964028dc0c508f383e7f9b44e98d
Gregory Markou <16929357+GregTheGreek@users.noreply.github.com>
2020-01-17 10:11:50 -0500

    Contracts (#61)
    * stuff
    
    * mods
    
    * changs
    
    * further changes
    
    * organise
    
    * update erc20 resolver
    
    * broke things
    
    * whitespace
    
    * fix tests
    
    * fix tests
    
    * rename contracts
    
    * address comments
    
    * address commnets
    
    * updated comments
    
    * update fn name

--- contracts/evm-contracts/contracts/Emitter.sol
+++ contracts/evm-contracts/contracts/Emitter.sol
@@ -1,10 +1,46 @@
 pragma solidity 0.5.12;
 
-contract Emitter {
-	event DepositAsset(address _addr, bytes32 indexed _hash);
+import "./Safe.sol";
 
-	function () external {
-		bytes32 hash = sha256(abi.encodePacked(msg.sender, block.number));
-		emit DepositAsset(msg.sender, hash);
-	}
+/**
+ * @title Emitter
+ * @dev The emitter is found on the origin chain,it is the on-chain
+ * user interacted portion of the bridge, and it prepares an asset for
+ * transfer. It is responsible for generating the necessary information
+ * for the bridge validators to successfully perform a transfer.
+ */
+ contract Emitter is Safe {
+
+    // ChainId => number of deposits
+    mapping(uint => uint) DepositCounts;
+
+    event GenericTransfer(uint _destChain, uint _depositId, address _destAddress, bytes _data);
+    event ERCTransfer(uint _destChain, uint _depositId, address _to, uint _amount, address _tokenAddress);
+    
+    constructor()
+        Safe(address(this)) 
+        public {}
+    
+    function deposit(uint _destChain, address _destAddress, bytes memory _data) public {
+        // Incremnet deposit
+        DepositCounts[_destChain]++;
+        uint depositId = DepositCounts[_destChain];
+        emit GenericTransfer(_destChain, depositId, _destAddress, _data);
+    }
+    
+    function depositGenericErc(
+        uint _destChain,
+        uint _value,
+        address _to,
+        address _tokenAddress
+    ) public {
+        // Incremnet deposit
+        DepositCounts[_destChain]++;
+        uint depositId = DepositCounts[_destChain];
+
+        // Lock funds
+        super.lockErc(_tokenAddress, _value, address(this), msg.sender);
+
+        emit ERCTransfer(_destChain, depositId, _to, _value, _tokenAddress);
+    }
 }
