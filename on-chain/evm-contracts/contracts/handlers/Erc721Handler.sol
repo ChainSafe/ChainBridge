2d0ddab71c75ff3275565757be7a79b1826fb6da
Gregory Markou <gregorymarkou@gmail.com>
2020-01-18 14:50:34 -0500

    update script paths

--- on-chain/evm-contracts/contracts/handlers/Erc721Handler.sol
+++ on-chain/evm-contracts/contracts/handlers/Erc721Handler.sol
@@ -15,15 +15,30 @@ contract ERC721Handler is Ownable, IHandler {
     mapping(uint => mapping(address => address)) public synthetics;
 
     function executeTransfer(uint _originChain, bytes calldata _data) public onlyOwner {
-        // @TODO unpack the data
-        /**
-         * _data = {
-           address _originAddress,
-           address _to,
-           uint _value,
-           bytes _extraData
-         }
-         */
+        // Decodes input bytes for _data struct
+        //     address _originAddress   @ 0x44
+        //     address _to              @ 0x64
+        //     uint _value              @ 0x84
+        //     bytes _extraData         @ 0xA4 ... arbitrary length
+        assembly {
+            // get addresses from calldata
+            origin := mload(add(0x20, _data))
+            to := mload(add(0x40, _data))
+
+            // get uint from calldata
+            value := mload(add(0x60, _data))
+
+            // get arbitrary length bytes from calldata
+            extra := mload(0x40)
+            let lenextra := mload(add(0x80, _data))
+            mstore(0x40, add(0x60, add(extra, lenextra)))
+            calldatacopy(
+                    extra,                     // copy to extra
+                    0xA4,                      // copy from calldata @ 0xA4
+                    sub(calldatasize, 0xA4)    // copy size (calldatasize - 0xA4)
+            )
+        }
+
 
         if (whitelist[_originChain][_originTokenAddress] != address(0)) {
             // Token is whitelisted
--- on-chain/evm-contracts/contracts/receiver.sol
+++ on-chain/evm-contracts/contracts/receiver.sol
@@ -196,18 +196,18 @@ contract Receiver {
         require(keccak256(_data) == proposal.hash, "Incorrect data supplied for hash");
 
         // TODO use generic receiver
-        // IHandler(_to).executeDeposit(_originChainId, _to, );
+        IHandler(_to).executeDeposit(_originChainId, _data);
 
-        ///////
-        // TODO remove this in favour of generic receiver
-        bytes32 centrifugeBytes32;
-        for (uint i = 0; i < 32; i++) {
-            centrifugeBytes32 |= bytes32(_data[i] & 0xFF) >> (i * 8);
-        }
-        // BridgeAsset centrifuge = BridgeAsset(_to);
-        BridgeAsset(_to).store(centrifugeBytes32);
-        // REMOVE ABOVE
-        ///////////////
+        // ///////
+        // // TODO remove this in favour of generic receiver
+        // bytes32 centrifugeBytes32;
+        // for (uint i = 0; i < 32; i++) {
+        //     centrifugeBytes32 |= bytes32(_data[i] & 0xFF) >> (i * 8);
+        // }
+        // // BridgeAsset centrifuge = BridgeAsset(_to);
+        // BridgeAsset(_to).store(centrifugeBytes32);
+        // // REMOVE ABOVE
+        // ///////////////
 
         // Mark deposit Transferred
         DepositProposals[_originChainId][_depositId].status = VoteStatus.Transferred;
