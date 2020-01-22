8ee2b677a0135adaa593a1afa060dccb31798638
noot <elizabethjbinks@gmail.com>
2020-01-22 12:05:56 -0500

    update binding script/makefile, add to readme

--- scripts/local_test/create_bindings.sh
+++ scripts/local_test/create_bindings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-contract_path="./contracts/evm-contracts/contracts/"
+contract_path="./contracts/evm-contracts/contracts"
 test_contracts="SimpleEmitter"
 contracts="Receiver BridgeAsset Emitter Safe"
 
@@ -10,6 +10,9 @@ pushd $contract_path
 
 # npm install --silent
 # node_modules/.bin/truffle compile
+
+rm -r ../bindings
+mkdir ../bindings
 
 for value in $test_contracts
 do
@@ -23,6 +26,7 @@ solcjs --abi -o abi/ Receiver.sol BridgeAsset.sol interfaces/IHandler.sol helper
 solcjs --bin -o bin/ Receiver.sol BridgeAsset.sol interfaces/IHandler.sol helpers/Ownable.sol 
 solcjs --abi -o abi/ Emitter.sol Safe.sol erc/ERC20.sol interfaces/ISafe.sol helpers/SafeMath.sol erc/IERC20.sol 
 solcjs --bin -o bin/ Emitter.sol Safe.sol erc/ERC20.sol interfaces/ISafe.sol helpers/SafeMath.sol erc/IERC20.sol 
+
 for value in $contracts
 do
 	abigen --abi abi/${value}_sol_${value}.abi --pkg $value --type $value --bin bin/${value}_sol_${value}.bin --out ../bindings/$value.go
@@ -33,6 +37,7 @@ popd
 
 for value in $test_contracts
 do
+	rm -r ./contracts/$value
 	mkdir "./contracts/$value"
 	mv "./contracts/evm-contracts/bindings/$value.go" "./contracts/$value"
 	echo "moved $value.go to go package ./contracts/$value"
@@ -40,6 +45,7 @@ done
 
 for value in $contracts
 do
+	rm -r ./contracts/$value
 	mkdir "./contracts/$value"
 	mv "./contracts/evm-contracts/bindings/$value.go" "./contracts/$value"
 	echo "moved $value.go to go package ./contracts/$value"
