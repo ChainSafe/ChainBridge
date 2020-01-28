9fbbb8634cbcc1ccdade4f9614c7178293bdd0cf
Linux Boot <anthony.chernyak@gmail.com>
2020-01-28 12:08:06 -0500

    Finishing Tests

--- keystore/keystore_test.go
+++ keystore/keystore_test.go
@@ -3,14 +3,13 @@ package keystore
 import (
 	"bytes"
 	"testing"
-
-	"github.com/status-im/keycard-go/hexutils"
 )
 
 func TestMockKeystore(t *testing.T) {
 	ks := NewTestKeystore("Alice")
+	tks := createTestKeyStore()
 	kp, _ := ks.KeypairFromAddress("Alice", "ethereum")
-	if !bytes.Equal(kp.Private().Encode(), hexutils.HexToBytes(TestEthereumPrivateKey)) {
+	if !bytes.Equal(kp.Private().Encode(), tks.Alice.SecpKeypair.Private().Encode()) {
 		t.Fatalf("unexpected key. got: %s expected: %s\n", kp.Private(), TestEthereumPrivateKey)
 	}
 }
