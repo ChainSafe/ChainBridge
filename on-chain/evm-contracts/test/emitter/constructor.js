72560c1fbdc1d1196de883018ecf875ba2ac9806
Gregory Markou <gregorymarkou@gmail.com>
2020-02-03 12:13:01 -0800

    format

---
+++ on-chain/evm-contracts/test/emitter/constructor.js
@@ -0,0 +1,16 @@
+const EmitterContract = artifacts.require("Emitter");
+
+contract('Emitter - [Deployment]', async (accounts) => {
+    let EmitterInstance;
+
+    // Set validators
+    let v1 = accounts[0];
+    let v2 = accounts[1];
+
+    before(async () => {
+        EmitterInstance = await EmitterContract.new();
+    });
+
+    it('should deploy safe', async () => {
+    });
+});
