6f1572309e332aac1c1c6f28e9badf5514d49141
Gregory Markou <gregorymarkou@gmail.com>
2020-02-06 12:33:41 -0500

    update per comments

--- chains/ethereum/config.go
+++ chains/ethereum/config.go
@@ -17,7 +17,8 @@ type Config struct {
 	from          string             // address of key to use
 	subscriptions []string           // list of events to subscribe to
 	keystore      *keystore.Keystore // Location of keyfiles
-	opts          configOpts         // General ethereum configuration options
+	receiver      common.Address
+	emitter       common.Address
 }
 
 type configOpts struct {
@@ -29,42 +30,28 @@ type configOpts struct {
 // ParseChainConfig uses a core.ChainConfig to construct a corresponding Config
 func ParseChainConfig(chainCfg *core.ChainConfig) *Config {
 
-	opts := parseOpts(chainCfg.Opts)
-
-	return &Config{
+	config := &Config{
 		id:            chainCfg.Id,
-		chainID:       opts.chainID,
 		endpoint:      chainCfg.Endpoint,
 		from:          chainCfg.From,
 		subscriptions: chainCfg.Subscriptions,
 		keystore:      chainCfg.Keystore,
-		opts:          opts,
+		chainID:       big.NewInt(1),
+		receiver:      common.HexToAddress("0x0"),
+		emitter:       common.HexToAddress("0x0"),
 	}
-}
 
-func parseOpts(opts map[string]string) configOpts {
-
-	config := defaultConfig()
-
-	if chainID, ok := opts["ChainID"]; ok {
+	if chainID, ok := chainCfg.Opts["ChainID"]; ok {
 		config.chainID.SetString(chainID, 10)
 	}
 
-	if receiver, ok := opts["Receiver"]; ok {
+	if receiver, ok := chainCfg.Opts["Receiver"]; ok {
 		config.receiver = common.HexToAddress(receiver)
 	}
 
-	if emitter, ok := opts["Emmitter"]; ok {
+	if emitter, ok := chainCfg.Opts["Emmitter"]; ok {
 		config.emitter = common.HexToAddress(emitter)
 	}
 
 	return config
-}
-
-func defaultConfig() configOpts {
-	return configOpts{
-		chainID:  big.NewInt(1),
-		receiver: common.HexToAddress("0x0"),
-		emitter:  common.HexToAddress("0x0"),
-	}
 }
