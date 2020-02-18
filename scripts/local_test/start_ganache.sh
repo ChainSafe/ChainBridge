03867d2e8096b108739be2fd61c6d7e76c31905b
Linux Boot <anthony.chernyak@gmail.com>
2020-02-18 18:33:31 -0500

    Silent ENV

--- scripts/local_test/start_ganache.sh
+++ scripts/local_test/start_ganache.sh
@@ -9,7 +9,7 @@ cd ./on-chain/evm-contracts &&
 npm install
 
 echo "Running ganache..."
-if [[ $1 = "silent" ]]; then
+if [[ $SILENT ]]; then
     node_modules/.bin/ganache-cli -p $PORT -m "when sound uniform light fee face forum huge impact talent exhaust arrow" > /dev/null &
 else
     node_modules/.bin/ganache-cli -p $PORT -m "when sound uniform light fee face forum huge impact talent exhaust arrow"
