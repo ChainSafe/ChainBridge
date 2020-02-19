53e22a723da8f7c1ff6ad4667b8dfdd8f136f589
Gregory Markou <gregorymarkou@gmail.com>
2020-02-19 14:35:49 -0500

    fix script

--- scripts/evm/create_go_bindings.sh
+++ scripts/evm/create_go_bindings.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 contract_path="on-chain/evm-contracts/contracts"
 contract_binding_path="on-chain/evm-contracts/"
-contracts="Receiver BridgeAsset Emitter Safe SimpleEmitter"
+contract_json_path=./on-chain/evm-contracts/contracts
 
 BIN_DIR=build/bindings/bin
 ABI_DIR=build/bindings/abi
 GO_DIR=build/bindings/go
 
-rm -rf ./build/bindings
-rm -rf ./build/bindings/go
-rm -rf ./contracts
-
 mkdir ./build/bindings
 mkdir ./build/bindings/go
-mkdir ./contracts
 
 cp -r $contract_binding_path/bindings/abi $ABI_DIR
 cp -r $contract_binding_path/bindings/bin $BIN_DIR
-
-contract_json_path=./on-chain/evm-contracts/contracts
 
 for file in "$BIN_DIR"/*.bin
 do
@@ -31,5 +26,9 @@ do
     mkdir ./contracts/$value
     
     # Build the go package
-    abigen --abi $ABI_DIR/${value}.abi --pkg $value --type $value --bin $BIN_DIR/${value}.bin --out contracts/$value/$value.go
+    abigen --abi $ABI_DIR/${value}.abi --pkg $value --type $value --bin $BIN_DIR/${value}.bin --out $GO_DIR/$value/$value.go
 done
+
+rm -rf ./contracts
+mkdir ./contracts
+cp -r $GO_DIR ./contracts
