f826514522dc6c372970780d02c8f4e599877de7
Wyatt Barnes <wyatt@writerof.software>
2020-02-20 11:24:30 -0600

    Replace magic address with variable for clarity

--- on-chain/evm-contracts/test/safe/constructor.js
+++ on-chain/evm-contracts/test/safe/constructor.js
@@ -1,16 +1,16 @@
 const SafeContract = artifacts.require("Safe");
 
 contract('Safe - [Deployment]', async (accounts) => {
+    const contractOwner = accounts[0];
+
     let SafeInstance;
 
     before(async () => {
-        SafeInstance = await SafeContract.new(
-            accounts[0] // contract owner 
-        )
+        SafeInstance = await SafeContract.new(contractOwner);
     });
 
     it('should set constructor values', async () => {
         let owner = await SafeInstance.owner.call();
-        assert.strictEqual(accounts[0], owner);
+        assert.strictEqual(contractOwner, owner);
     });
 });
