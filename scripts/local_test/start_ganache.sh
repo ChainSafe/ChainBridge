ca4e6d2189f4c0217ce82525f630dad0a2201d7a
Linux Boot <anthony.chernyak@gmail.com>
2020-02-20 15:45:58 -0500

    Adding quotes

--- scripts/local_test/start_ganache.sh
+++ scripts/local_test/start_ganache.sh
@@ -5,7 +5,7 @@ set -e
 
 PORT=${PORT:-8545}
 
-MNEMONIC="when sound uniform light fee face forum huge impact talent exhaust arrow"
+MNEMONIC="\"when sound uniform light fee face forum huge impact talent exhaust arrow\""
 
 cd ./on-chain/evm-contracts &&
 npm install
