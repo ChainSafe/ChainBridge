8da85406faea0b11e42dcd422265c618edd2e672
Gregory Markou <gregorymarkou@gmail.com>
2020-02-22 20:42:22 -0500

    better logging, and remove id.string()

--- message/id.go
+++ message/id.go
@@ -5,13 +5,13 @@ type ChainId uint8
 const EthereumId = ChainId(0)
 const CentrifugeId = ChainId(1)
 
-func (id ChainId) String() string {
-	switch id {
-	case EthereumId:
-		return "ethereum"
-	case CentrifugeId:
-		return "centrifuge"
-	default:
-		return "unknown"
-	}
-}
+// func (id ChainId) String() string {
+// 	switch id {
+// 	case EthereumId:
+// 		return "ethereum"
+// 	case CentrifugeId:
+// 		return "centrifuge"
+// 	default:
+// 		return "unknown"
+// 	}
+// }
