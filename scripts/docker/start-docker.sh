03be4ea029e6e92b02aa33875ef662d354fd0099
Linux Boot <anthony.chernyak@gmail.com>
2020-02-24 16:52:52 -0500

    Missed a reference

--- scripts/docker/start-docker.sh
+++ scripts/docker/start-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MNEMONIC=${MNEMONIC} docker build -f ./Docker/Ganache.Dockerfile -t 'chainbridgev2_ganache:latest' .
+docker build -f ./Docker/Ganache.Dockerfile -t 'chainbridgev2_ganache:latest' .
 docker run -d -p 8545:8545 -p 8546:8546 chainbridgev2_ganache
 
 # docker exec -it chainbridgev2_ganache node ./scripts/deploy_local.js --validators 3
