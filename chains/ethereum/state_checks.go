447bf847d6d8362eb73dd2c7c0540075e05065fa
RyRy79261 <ryanjnoble@gmail.com>
2020-02-27 16:58:45 +0200

    Reworked code to avoid contract interface

--- chains/ethereum/state_checks.go
+++ chains/ethereum/state_checks.go
@@ -3,24 +3,26 @@ package ethereum
 import (
 	"math/big"
 
-	"github.com/ChainSafe/ChainBridgeV2/contracts/Receiver"
 	"github.com/ChainSafe/log15"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 // Queries the contract for the current deposit status
 func (w *Writer) DepositStatus(depositId *big.Int, originChain *big.Int) (uint8, error) {
-	caller, err := Receiver.NewReceiverCaller(w.cfg.receiver, w.conn.conn)
-	if err != nil {
-		log15.Error("failed to init caller instance", "error", err)
-		return 0, err
-	}
-
-	ret, err := caller.DepositProposals(new(bind.CallOpts), depositId, originChain)
+	ret := new(struct {
+		Hash        [32]byte
+		Id          *big.Int
+		OriginChain *big.Int
+		NumYes      *big.Int
+		NumNo       *big.Int
+		Status      uint8
+	})
+	out := ret
+	err := w.receiverContract.Call(new(bind.CallOpts), out, "DepositProposals", depositId, originChain)
 	if err != nil {
 		log15.Error("failed to call DepositProposal", "error", err)
 		return 0, err
 	}
 
-	return ret.Status, nil
+	return out.Status, err
 }
