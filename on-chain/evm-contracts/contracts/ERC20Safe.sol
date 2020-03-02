cc32f057321cbdaa32b4c1d19e9dc864db7ab76d
Wyatt Barnes <wyatt@writerof.software>
2020-03-02 15:07:34 -0600

    Replace usage of ERC20 contract with IERC20. Add releaseERC20 method

--- on-chain/evm-contracts/contracts/ERC20Safe.sol
+++ on-chain/evm-contracts/contracts/ERC20Safe.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.12;
 
 import "./helpers/SafeMath.sol";
-import "./erc/ERC20/ERC20.sol";
+import "./erc/ERC20/IERC20.sol";
 
 contract ERC20Safe {
     using SafeMath for uint;
@@ -10,11 +10,16 @@ contract ERC20Safe {
     mapping(address => uint) public _balances;
 
     function lockERC20(address tokenAddress, address owner, address recipient, uint amount) internal {
-        ERC20 erc20 = ERC20(tokenAddress);
+        IERC20 erc20 = IERC20(tokenAddress);
         erc20.transferFrom(owner, recipient, amount);
 
         _balances[tokenAddress] = _balances[tokenAddress].add(amount);
     }
 
-    // function releaseERC20();
+    function releaseERC20(address tokenAddress, address owner, address recipient, uint amount) internal {
+        IERC20 erc20 = IERC20(tokenAddress);
+        erc20.transferFrom(owner, recipient, amount);
+
+        _balances[tokenAddress] = _balances[tokenAddress].sub(amount);
+    }
 }
