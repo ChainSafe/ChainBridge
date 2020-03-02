37baa6acf15fdeea0030340b433286276d82f51c
Wyatt Barnes <wyatt@writerof.software>
2020-03-02 15:09:52 -0600

    Add ValidatorActionType, ThresholdType, and Vote enums. Add hasVoted, createValidatorProposal, voteValidatorProposal, createThresholdProposal, and voteThresholdProposal function signatures

--- on-chain/evm-contracts/contracts/interfaces/IValidator.sol
+++ on-chain/evm-contracts/contracts/interfaces/IValidator.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.5.12;
 
 interface IValidator {
+    enum ValidatorActionType {Add, Remove}
+    enum ThresholdType {}
+    enum Vote {No, Yes}
+
     function isValidator(address validatorAddress) external returns (bool);
     function getTotalValidators() external returns (uint);
+    function hasVoted(uint depositID, address validatorAddress) external returns (bool);
+    function createValidatorProposal(address validatorAddress, ValidatorActionType action) external;
+    function voteValidatorProposal(address validatorAddress, Vote vote) external;
+    function createThresholdProposal(uint value, ThresholdType type) external;
+    function voteThresholdProposal(Vote vote, ThresholdType type) external;
 }
