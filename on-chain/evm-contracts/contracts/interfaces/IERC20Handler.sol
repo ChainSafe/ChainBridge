4c7545c55abd220552526c39d5bed4989c45c086
Wyatt Barnes <wyatt@writerof.software>
2020-03-02 15:08:43 -0600

    Remove executeDeposit. Add withdrawERC20

--- on-chain/evm-contracts/contracts/interfaces/IERC20Handler.sol
+++ on-chain/evm-contracts/contracts/interfaces/IERC20Handler.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.5.12;
 
 interface IERC20Handler {
     function depositERC20(address tokenAddress, address owner, uint amount) external;
-    function executeDeposit(bytes calldata data) external;
+    function withdrawERC20(address tokenAddress, address recipient, uint amount) external;
 }
