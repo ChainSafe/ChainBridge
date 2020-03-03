e2685b041f63053c35356aab3ce64af7f599e3c7
Wyatt Barnes <wyatt@writerof.software>
2020-03-03 00:50:31 -0600

    Add createValidatorProposal

--- on-chain/evm-contracts/contracts/Validator.sol
+++ on-chain/evm-contracts/contracts/Validator.sol
@@ -7,39 +7,66 @@ contract Validator is IValidator {
     uint public _validatorThreshold;
     uint public _totalValidators;
 
-    enum Vote {Yes, No}
-    enum ValidatorActionType {Add, Remove}
-    enum VoteStatus {Inactive, Active, Finalized, Transferred}
-    enum ThresholdType {Validator, Deposit}
-
     struct ValidatorProposal {
-        // Address of the proposed validator
-        address validator;
-        // validator action
-        ValidatorActionType action;
-        // Keeps track if a user has voted
-        mapping(address => bool) votes;
-        // Number of votes in favour
-        uint numYes;
-        // Number of votes against
-        uint numNo;
-        // Status of a vote
-        VoteStatus status;
+        address                  _proposedAddress;
+        ValidatorActionType      _action;
+        mapping(address => bool) _votes;
+        uint                     _numYes;
+        uint                     _numNo;
+        VoteStatus               _status;
     }
 
-    mapping(address => bool) public Validators;
-    mapping(address => ValidatorProposal) public ValidatorProposals;
+    // Validator Address => whether they are a Validator
+    mapping(address => bool) public _validators;
+    // Validator Address => ValidatorProposal
+    mapping(address => ValidatorProposal) public _validatorProposals;
 
     modifier _onlyValidators() {
-        require(Validators[msg.sender], "sender is not a validator");
+        require(_validators[msg.sender], "sender is not a validator");
         _;
     }
 
     function isValidator(address validatorAddress) public returns (bool) {
-        return Validators[validatorAddress];
+        return _validators[validatorAddress];
     }
 
     function getTotalValidators() public returns (uint) {
         return _totalValidators;
     }
+
+    function createValidatorProposal(address proposedAddress, ValidatorActionType action) public {
+        require(uint(action) <= 1, "action out of the vote enum range");
+        require(action == ValidatorActionType.Remove && _validators[proposedAddress] == true, "address is not a validator");
+        require(action == ValidatorActionType.Add && _validators[proposedAddress] == false, "address is currently a validator");
+        require(_validatorProposals[proposedAddress]._status == VoteStatus.Inactive, "there is already an active proposal for this address");
+
+        _validatorProposals[proposedAddress] = ValidatorProposal({
+            _proposedAddress: proposedAddress,
+            _action: action,
+            _numYes: 1, // Creator always votes in favour
+            _numNo: 0,
+            _status: VoteStatus.Active
+            });
+
+        if (_validatorThreshold == 1) {
+            _validatorProposals[proposedAddress]._status = VoteStatus.Inactive;
+            if (action == ValidatorActionType.Add) {
+                // Add validator
+                _validators[proposedAddress] = true;
+                _totalValidators++;
+            } else {
+                // Remove validator
+                _validators[proposedAddress] = false;
+                _totalValidators--;
+            }
+        }
+        // Record vote
+        _validatorProposals[proposedAddress]._votes[msg.sender] = true;
+    }
+
+    // function voteValidatorProposal(address validatorAddress, Vote vote) external;
+
+    // function createThresholdProposal(uint value, ThresholdType thresholdType) external;
+
+    // function voteThresholdProposal(Vote vote, ThresholdType thresholdType) external;
 }
