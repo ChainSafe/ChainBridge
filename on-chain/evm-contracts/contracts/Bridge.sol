f5b9683ad3140ab1e43c9858d3e58454ae026955
Wyatt Barnes <wyatt@writerof.software>
2020-03-03 03:05:33 -0600

    Add createValidatorThresholdProposal method

--- on-chain/evm-contracts/contracts/Bridge.sol
+++ on-chain/evm-contracts/contracts/Bridge.sol
@@ -9,14 +9,15 @@ import "./interfaces/IDepositHandler.sol";
 contract Bridge {
     using SafeMath for uint;
 
-    address public _validatorContract;
-    uint public _depositThreshold;
+    IValidator public _validatorContract;
+    uint public _validatorThreshold;
+    ValidatorThresholdProposal public _currentValidatorThresholdProposal;
 
     enum Vote {No, Yes}
-    enum ProposalStatus {Inactive, Active, Denied, Passed, Transferred}
-    enum ThresholdType {Validator, Deposit}
+    enum ValidatorThresholdProposalStatus {Inactive, Active}
+    enum DepositProposalStatus {Inactive, Active, Denied, Passed, Transferred}
 
-    struct GenericDepositRecord {
+    struct GenricDepositRecord {
         address _originChainTokenAddress;
         address _originChainHandlerAddress;
         uint    _destinationChainID;
@@ -51,13 +52,21 @@ contract Bridge {
         mapping(address => bool) _votes;
         uint _numYes;
         uint _numNo;
-        ProposalStatus _status;
+        DepositProposalStatus _status;
+    }
+
+    struct ValidatorThresholdProposal {
+        uint                             _proposedValue;
+        mapping(address => bool)         _votes;
+        uint                             _numYes;
+        uint                             _numNo;
+        ValidatorThresholdProposalStatus _status;
     }
 
     // chainID => number of deposits
     mapping(uint => uint) public _depositCounts;
-    // chainID => depositID => GenericDepositRecord
-    mapping(uint => mapping(uint => GenericDepositRecord)) public _genericDepositRecords;
+    // chainID => depositID => GenricDepositRecord
+    mapping(uint => mapping(uint => GenricDepositRecord)) public _genericDepositRecords;
     // chainID => depositID => ERC20DepositRecord
     mapping(uint => mapping(uint => ERC20DepositRecord)) public _erc20DepositRecords;
     // chainID => depositID => ERC721DepositRecord
@@ -69,13 +78,17 @@ contract Bridge {
     event ERC20Deposited(uint indexed depositID);
     event ERC721Deposited(uint indexed depositID);
     event DepositProposalCreated(uint indexed originChainID, uint indexed depositID, bytes32 indexed dataHash);
-    event DepositProposalVote(uint indexed originChainID, uint indexed depositID, Vote indexed vote, ProposalStatus status);
+    event DepositProposalVote(uint indexed originChainID, uint indexed depositID, Vote indexed vote, DepositProposalStatus status);
     event DepositProposalFinalized(uint indexed originChainID, uint indexed depositID);
 
     modifier _onlyValidators() {
         IValidator validatorContract = IValidator(_validatorContract);
         require(validatorContract.isValidator(msg.sender));
         _;
+    }
+
+    constructor (address validatorContract) public {
+        _validatorContract = IValidator(validatorContract);
     }
 
     function hasVoted(uint originChainID, uint depositID, address validatorAddress) public view returns (bool) {
@@ -89,7 +102,7 @@ contract Bridge {
     ) public {
         uint depositID = _depositCounts[destinationChainID]++;
 
-        _genericDepositRecords[destinationChainID][depositID] = GenericDepositRecord(
+        _genericDepositRecords[destinationChainID][depositID] = GenricDepositRecord(
             address(0),
             address(0),
             destinationChainID,
@@ -111,7 +124,7 @@ contract Bridge {
     ) public {
         uint depositID = _depositCounts[destinationChainID]++;
 
-        _genericDepositRecords[destinationChainID][depositID] = GenericDepositRecord(
+        _genericDepositRecords[destinationChainID][depositID] = GenricDepositRecord(
             originChainContractAddress,
             originChainHandlerAddress,
             destinationChainID,
@@ -176,17 +189,18 @@ contract Bridge {
     }
 
     function createDepositProposal(uint originChainID, uint depositID, bytes32 dataHash) public _onlyValidators {
-        require(_depositProposals[originChainID][depositID]._status == ProposalStatus.Inactive, "this proposal already exists");
+        require(_depositProposals[originChainID][depositID]._status == DepositProposalStatus.Inactive ||
+        _depositProposals[originChainID][depositID]._status == DepositProposalStatus.Denied, "this proposal is either currently active or has already been passed/transferred");
 
         // If _depositThreshold is set to 1, then auto finalize
-        if (_depositThreshold == 1) {
+        if (_validatorThreshold <= 1) {
             _depositProposals[originChainID][depositID] = DepositProposal({
                 _originChainID: originChainID,
                 _depositID: depositID,
                 _dataHash: dataHash,
                 _numYes: 1, // Creator always votes in favour
                 _numNo: 0,
-                _status: ProposalStatus.Passed
+                _status: DepositProposalStatus.Passed
                 });
         } else {
             _depositProposals[originChainID][depositID] = DepositProposal({
@@ -195,7 +209,7 @@ contract Bridge {
                 _dataHash: dataHash,
                 _numYes: 1, // Creator always votes in favour
                 _numNo: 0,
-                _status: ProposalStatus.Active
+                _status: DepositProposalStatus.Active
                 });
         }
 
@@ -208,8 +222,8 @@ contract Bridge {
     function voteDepositProposal(uint originChainID, uint depositID, Vote vote) public _onlyValidators {
         DepositProposal storage depositProposal = _depositProposals[originChainID][depositID];
 
-        require(depositProposal._status != ProposalStatus.Inactive, "proposal is not active");
-        require(depositProposal._status != ProposalStatus.Active, "proposal has been finalized");
+        require(depositProposal._status != DepositProposalStatus.Inactive, "proposal is not active");
+        require(depositProposal._status == DepositProposalStatus.Active, "proposal has been finalized");
         require(!depositProposal._votes[msg.sender], "validator has already voted");
         require(uint(vote) <= 1, "invalid vote");
 
@@ -221,14 +235,12 @@ contract Bridge {
 
         depositProposal._votes[msg.sender] = true;
 
-        IValidator validatorContract = IValidator(_validatorContract);
-
         // Todo: Edge case if validator threshold changes?
-        if (depositProposal._numYes >= _depositThreshold) {
-            depositProposal._status = ProposalStatus.Passed;
+        if (depositProposal._numYes >= _validatorThreshold) {
+            depositProposal._status = DepositProposalStatus.Passed;
             emit DepositProposalFinalized(originChainID, depositID);
-        } else if (validatorContract.getTotalValidators().sub(depositProposal._numNo) < _depositThreshold) {
-            depositProposal._status = ProposalStatus.Denied;
+        } else if (_validatorContract.getTotalValidators().sub(depositProposal._numNo) < _validatorThreshold) {
+            depositProposal._status = DepositProposalStatus.Denied;
             emit DepositProposalFinalized(originChainID, depositID);
         }
 
@@ -238,16 +250,34 @@ contract Bridge {
     function executeDepositProposal(uint originChainID, uint depositID, address destinationChainHandlerAddress, bytes memory data) public {
         DepositProposal storage depositProposal = _depositProposals[originChainID][depositID];
 
-        require(depositProposal._status == ProposalStatus.Passed, "proposal was not passed");
+        require(depositProposal._status != DepositProposalStatus.Inactive, "proposal is not active");
+        require(depositProposal._status == DepositProposalStatus.Passed, "proposal was not passed or has already been transferred");
         require(keccak256(data) == depositProposal._dataHash, "provided data does not match proposal's data hash");
 
         IDepositHandler depositHandler = IDepositHandler(destinationChainHandlerAddress);
         depositHandler.executeDeposit(data);
 
-        depositProposal._status = ProposalStatus.Transferred;
+        depositProposal._status = DepositProposalStatus.Transferred;
     }
 
-//    function createThresholdProposal(uint value) external;
-//
-//    function voteThresholdProposal(Vote vote) external;
+    function createValidatorThresholdProposal(uint proposedValue) public _onlyValidators {
+        require(_currentValidatorThresholdProposal._status == ValidatorThresholdProposalStatus.Inactive, "a proposal is currently active");
+        require(proposedValue <= _validatorContract.getTotalValidators(), "proposed value cannot be greater than the total number of validators");
+
+        _currentValidatorThresholdProposal = ValidatorThresholdProposal({
+            _proposedValue: proposedValue,
+            _numYes: 1, // Creator always votes in favour
+            _numNo: 0,
+            _status: ValidatorThresholdProposalStatus.Active
+            });
+
+        if (_validatorThreshold <= 1) {
+            _validatorThreshold = _currentValidatorThresholdProposal._proposedValue;
+            _currentValidatorThresholdProposal._status = ValidatorThresholdProposalStatus.Inactive;
+        }
+        // Record vote
+        _currentValidatorThresholdProposal._votes[msg.sender] = true;
+    }
+
+    // function voteThresholdProposal(Vote vote) external;
 }
