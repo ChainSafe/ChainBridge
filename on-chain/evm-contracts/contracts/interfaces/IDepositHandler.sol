8945c7d0e04650bf74384046bdd9c4e609029b74
Wyatt Barnes <wyatt@writerof.software>
2020-03-13 01:30:32 -0500

    Add deposit method

--- on-chain/evm-contracts/contracts/interfaces/IDepositHandler.sol
+++ on-chain/evm-contracts/contracts/interfaces/IDepositHandler.sol
@@ -1,5 +1,6 @@
-pragma solidity ^0.5.12;
+pragma solidity 0.6.1;
 
 interface IDepositHandler {
+    function deposit(uint256 depositID, bytes calldata data) external;
     function executeDeposit(bytes calldata data) external;
 }
