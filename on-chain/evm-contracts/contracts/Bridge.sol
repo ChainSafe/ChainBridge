cc3a6ae15c94c09afc650feebf273c46778dc77a
Wyatt Barnes <wyatt@writerof.software>
2020-03-13 01:29:52 -0500

    Replace specific deposit methods with generic method

--- on-chain/evm-contracts/contracts/Bridge.sol
+++ on-chain/evm-contracts/contracts/Bridge.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.12;
+pragma solidity 0.6.1;
+pragma experimental ABIEncoderV2;
 
 import "./helpers/SafeMath.sol";
 import "./interfaces/IValidator.sol";
@@ -9,92 +10,70 @@ import "./interfaces/IDepositHandler.sol";
 contract Bridge {
     using SafeMath for uint;
 
-    IValidator public _validatorContract;
-    uint public _validatorThreshold;
+    IValidator                 public _validatorContract;
+    uint256                    public _validatorThreshold;
     ValidatorThresholdProposal public _currentValidatorThresholdProposal;
 
     enum Vote {No, Yes}
-
-    // ValidatorThresholdProposalStatus and _validatorThresholdProposalStatusStrings must be kept
-    // the same length and order to function properly
     enum ValidatorThresholdProposalStatus {Inactive, Active}
-    string[] _validatorThresholdProposalStatusStrings = ["inactive", "active"];
-
-    // DepositProposalStatus and _depositProposalStatusStrings must be kept
-    // the same length and order to function properly
     enum DepositProposalStatus {Inactive, Active, Denied, Passed, Transferred}
-    string[] _depositProposalStatusStrings = ["inactive", "active", "denied", "passed", "transferred"];
 
-    struct GenericDepositRecord {
-        address _originChainTokenAddress;
-        address _originChainHandlerAddress;
-        uint    _destinationChainID;
-        address _destinationChainHandlerAddress;
-        address _destinationRecipientAddress;
+    struct DepositRecord {
         bytes   _data;
     }
-
-    struct ERC20DepositRecord {
-        address _originChainTokenAddress;
-        address _originChainHandlerAddress;
-        uint    _destinationChainID;
-        address _destinationChainHandlerAddress;
-        address _destinationRecipientAddress;
-        uint    _amount;
-    }
-
-    struct ERC721DepositRecord {
-        address _originChainTokenAddress;
-        address _originChainHandlerAddress;
-        uint    _destinationChainID;
-        address _destinationChainHandlerAddress;
-        address _destinationRecipientAddress;
-        uint    _tokenID;
-        bytes   _data;
-    }
-
     struct DepositProposal {
-        uint _originChainID;
-        uint _depositID;
-        bytes32 _dataHash;
+        bytes32                  _dataHash;
         mapping(address => bool) _votes;
-        uint _numYes;
-        uint _numNo;
-        DepositProposalStatus _status;
+        uint8                    _numYes;
+        uint8                    _numNo;
+        DepositProposalStatus    _status;
     }
-
     struct ValidatorThresholdProposal {
-        uint                             _proposedValue;
+        uint256                          _proposedValue;
         mapping(address => bool)         _votes;
-        uint                             _numYes;
-        uint                             _numNo;
+        uint8                            _numYes;
+        uint8                            _numNo;
         ValidatorThresholdProposalStatus _status;
     }
 
-    // chainID => number of deposits
-    mapping(uint => uint) public _depositCounts;
-    // chainID => depositID => GenericDepositRecord
-    mapping(uint => mapping(uint => GenericDepositRecord)) public _genericDepositRecords;
-    // chainID => depositID => ERC20DepositRecord
-    mapping(uint => mapping(uint => ERC20DepositRecord)) public _erc20DepositRecords;
-    // chainID => depositID => ERC721DepositRecord
-    mapping(uint => mapping(uint => ERC721DepositRecord)) public _erc721DepositRecords;
-    // ChainId => DepositID => Proposal
-    mapping(uint => mapping(uint => DepositProposal)) public _depositProposals;
+    // handler address => number of deposits
+    mapping(address => uint256) public _depositCounts;
+    // handler address => depositID => DepositRecord
+    mapping(address => mapping(uint256 => DepositRecord)) public _depositRecords;
+    // originChainID => handler address => depositID => depositProposal
+    mapping(uint256 => mapping(address => mapping(uint256 => DepositProposal))) public _depositProposals;
 
-    event GenericDeposited(uint indexed depositID);
-    event ERC20Deposited(uint indexed depositID);
-    event ERC721Deposited(uint indexed depositID);
-    event DepositProposalCreated(uint indexed originChainID, uint indexed depositID, bytes32 indexed dataHash);
-    event DepositProposalVote(uint indexed originChainID, uint indexed depositID, Vote indexed vote, DepositProposalStatus status);
-    event DepositProposalFinalized(uint indexed originChainID, uint indexed depositID);
     event ValidatorThresholdProposalCreated(uint indexed proposedValue);
     event ValidatorThresholdProposalVote(Vote vote);
     event ValidatorThresholdChanged(uint indexed newThreshold);
+    event Deposit(address indexed handlerAddress, uint256 indexed depositID);
+    event DepositProposalCreated(
+        uint256 indexed originChainID,
+        address indexed originChainHandlerAddress,
+        uint256 indexed depositID,
+        bytes32         dataHash
+    );
+    event DepositProposalVote(
+        uint256 indexed       originChainID,
+        address indexed       originChainHandlerAddress,
+        uint256 indexed       depositID,
+        Vote                  vote,
+        DepositProposalStatus status
+    );
+    event DepositProposalFinalized(
+        uint256 indexed originChainID,
+        address indexed originChainHandlerAddress,
+        uint256 indexed depositID
+    );
+    event DepositProposalExecuted(
+        uint256 indexed originChainID,
+        address indexed originChainHandlerAddress,
+        uint256 indexed depositID
+    );
 
     modifier _onlyValidators() {
         IValidator validatorContract = IValidator(_validatorContract);
-        require(validatorContract.isValidator(msg.sender));
+        require(validatorContract.isValidator(msg.sender), "sender is not a validator");
         _;
     }
 
@@ -103,202 +82,99 @@ contract Bridge {
         _validatorThreshold = initialValidatorThreshold;
     }
 
-    function getValidatorThreshold() public view returns (uint) {
+    function getValidatorThreshold() public view returns (uint256) {
         return _validatorThreshold;
     }
 
-    function getDepositCount(uint originChainID) public view returns (uint) {
+    function getCurrentValidatorThresholdProposal() public view returns (ValidatorThresholdProposal memory) {
+        return _currentValidatorThresholdProposal;
+    }
+
+    function getDepositCount(uint originChainID) public view returns (uint256) {
         return _depositCounts[originChainID];
     }
 
-    function getGenericDepositRecord(uint originChainID, uint depositID) public view returns (
-        address, address, uint, address, address, bytes memory) {
-        GenericDepositRecord memory genericDepositRecord = _genericDepositRecords[originChainID][depositID];
-        return (
-            genericDepositRecord._originChainTokenAddress,
-            genericDepositRecord._originChainHandlerAddress,
-            genericDepositRecord._destinationChainID,
-            genericDepositRecord._destinationChainHandlerAddress,
-            genericDepositRecord._destinationRecipientAddress,
-            genericDepositRecord._data);
+    function getDepositRecord(address handlerAddress, uint256 depositID) public view returns (DepositRecord memory) {
+        return _depositRecords[handlerAddress][depositID];
     }
 
-    function getERC20DepositRecord(uint originChainID, uint depositID) public view returns (
-        address, address, uint, address, address, uint) {
-        ERC20DepositRecord memory erc20DepositRecord = _erc20DepositRecords[originChainID][depositID];
-        return (
-            erc20DepositRecord._originChainTokenAddress,
-            erc20DepositRecord._originChainHandlerAddress,
-            erc20DepositRecord._destinationChainID,
-            erc20DepositRecord._destinationChainHandlerAddress,
-            erc20DepositRecord._destinationRecipientAddress,
-            erc20DepositRecord._amount);
-    }
-
-    function getERC721DepositRecord(uint originChainID, uint depositID) public view returns (
-        address, address, uint, address, address, uint, bytes memory) {
-        ERC721DepositRecord memory erc721DepositRecord = _erc721DepositRecords[originChainID][depositID];
-        return (
-            erc721DepositRecord._originChainTokenAddress,
-            erc721DepositRecord._originChainHandlerAddress,
-            erc721DepositRecord._destinationChainID,
-            erc721DepositRecord._destinationChainHandlerAddress,
-            erc721DepositRecord._destinationRecipientAddress,
-            erc721DepositRecord._tokenID,
-            erc721DepositRecord._data);
-    }
-
-    function getCurrentValidatorThresholdProposal() public view returns (
-        uint, uint, uint, string memory) {
-        return (
-            _currentValidatorThresholdProposal._proposedValue,
-            _currentValidatorThresholdProposal._numYes,
-            _currentValidatorThresholdProposal._numNo,
-            _validatorThresholdProposalStatusStrings[uint(_currentValidatorThresholdProposal._status)]);
-    }
-
-    function getDepositProposal(uint originChainID, uint depositID) public view returns (
-        uint, uint, bytes32, uint, uint, string memory) {
-        DepositProposal memory depositProposal = _depositProposals[originChainID][depositID];
-        return (
-            depositProposal._originChainID,
-            depositProposal._depositID,
-            depositProposal._dataHash,
-            depositProposal._numYes,
-            depositProposal._numNo,
-            _depositProposalStatusStrings[uint(depositProposal._status)]);
-    }
-
-    function hasVoted(uint originChainID, uint depositID, address validatorAddress) public view returns (bool) {
-        return _depositProposals[originChainID][depositID]._votes[validatorAddress];
-    }
-
-    function depositGeneric(
-        uint         destinationChainID,
-        address      destinationRecipientAddress,
-        bytes memory data
-    ) public {
-        uint depositID = ++_depositCounts[destinationChainID];
-
-        _genericDepositRecords[destinationChainID][depositID] = GenericDepositRecord(
-            address(0),
-            address(0),
-            destinationChainID,
-            address(0),
-            destinationRecipientAddress,
-            data
-        );
-
-        emit GenericDeposited(depositID);
-    }
-
-    function depositGeneric(
-        address      originChainContractAddress,
-        address      originChainHandlerAddress,
-        uint         destinationChainID,
-        address      destinationChainHandlerAddress,
-        address      destinationRecipientAddress,
-        bytes memory data
-    ) public {
-        uint depositID = ++_depositCounts[destinationChainID];
-
-        _genericDepositRecords[destinationChainID][depositID] = GenericDepositRecord(
-            originChainContractAddress,
-            originChainHandlerAddress,
-            destinationChainID,
-            destinationChainHandlerAddress,
-            destinationRecipientAddress,
-            data
-        );
-
-        emit GenericDeposited(depositID);
-    }
-
-    function depositERC20(
-        address originChainTokenAddress,
+    function getDepositProposal(
+        uint256 originChainID,
         address originChainHandlerAddress,
-        uint    destinationChainID,
-        address destinationChainHandlerAddress,
-        address destinationRecipientAddress,
-        uint    amount
-    ) public {
-        IERC20Handler erc20Handler = IERC20Handler(originChainHandlerAddress);
-        erc20Handler.depositERC20(originChainTokenAddress, msg.sender, amount);
-
-        uint depositID = ++_depositCounts[destinationChainID];
-
-        _erc20DepositRecords[destinationChainID][depositID] = ERC20DepositRecord(
-            originChainTokenAddress,
-            originChainHandlerAddress,
-            destinationChainID,
-            destinationChainHandlerAddress,
-            destinationRecipientAddress,
-            amount
-        );
-
-        emit ERC20Deposited(depositID);
+        uint256 depositID
+    ) public view returns (DepositProposal memory) {
+        return _depositProposals[originChainID][originChainHandlerAddress][depositID];
     }
 
-    function depositERC721(
-        address      originChainTokenAddress,
-        address      originChainHandlerAddress,
-        uint         destinationChainID,
-        address      destinationChainHandlerAddress,
-        address      destinationRecipientAddress,
-        uint         tokenID,
+    function hasVoted(
+        uint256 originChainID,
+        address originChainHandlerAddress,
+        uint256 depositID,
+        address validatorAddress
+    ) public view returns (bool) {
+        return _depositProposals[originChainID][originChainHandlerAddress][depositID]._votes[validatorAddress];
+    }
+
+    function deposit(
+        address      handlerAddress,
         bytes memory data
     ) public {
-        IERC721Handler erc721Handler = IERC721Handler(originChainHandlerAddress);
-        erc721Handler.depositERC721(originChainTokenAddress, msg.sender, tokenID);
+        uint depositID = ++_depositCounts[handlerAddress];
 
-        uint depositID = ++_depositCounts[destinationChainID];
-
-        _erc721DepositRecords[destinationChainID][depositID] = ERC721DepositRecord(
-            originChainTokenAddress,
-            originChainHandlerAddress,
-            destinationChainID,
-            destinationChainHandlerAddress,
-            destinationRecipientAddress,
-            tokenID,
+        _depositRecords[handlerAddress][depositID] = DepositRecord(
             data
         );
 
-        emit ERC721Deposited(depositID);
+        IDepositHandler depositHandler = IDepositHandler(handlerAddress);
+        depositHandler.deposit(depositID, data);
+
+        emit Deposit(handlerAddress, depositID);
     }
 
-    function createDepositProposal(uint originChainID, uint depositID, bytes32 dataHash) public _onlyValidators {
-        require(_depositProposals[originChainID][depositID]._status == DepositProposalStatus.Inactive ||
-        _depositProposals[originChainID][depositID]._status == DepositProposalStatus.Denied, "this proposal is either currently active or has already been passed/transferred");
+    function createDepositProposal(
+        uint256 originChainID,
+        address originChainHandlerAddress,
+        uint256 depositID,
+        bytes32 dataHash
+    ) public _onlyValidators {
+        DepositProposal storage depositProposal = _depositProposals[originChainID][originChainHandlerAddress][depositID];
+
+        require(
+            depositProposal._status == DepositProposalStatus.Inactive ||
+            depositProposal._status == DepositProposalStatus.Denied,
+            "proposal is either currently active or has already been passed/transferred"
+        );
 
         // If _depositThreshold is set to 1, then auto finalize
         if (_validatorThreshold <= 1) {
-            _depositProposals[originChainID][depositID] = DepositProposal({
-                _originChainID: originChainID,
-                _depositID: depositID,
+            depositProposal = DepositProposal({
                 _dataHash: dataHash,
                 _numYes: 1, // Creator always votes in favour
                 _numNo: 0,
                 _status: DepositProposalStatus.Passed
-                });
+            });
         } else {
-            _depositProposals[originChainID][depositID] = DepositProposal({
-                _originChainID: originChainID,
-                _depositID: depositID,
+            depositProposal = DepositProposal({
                 _dataHash: dataHash,
                 _numYes: 1, // Creator always votes in favour
                 _numNo: 0,
                 _status: DepositProposalStatus.Active
-                });
+            });
         }
 
         // Creator always votes in favour
-        _depositProposals[originChainID][depositID]._votes[msg.sender] = true;
+        depositProposal._votes[msg.sender] = true;
 
-        emit DepositProposalCreated(originChainID, depositID, dataHash);
+        emit DepositProposalCreated(originChainID, originChainHandlerAddress, depositID, dataHash);
     }
 
-    function voteDepositProposal(uint originChainID, uint depositID, Vote vote) public _onlyValidators {
-        DepositProposal storage depositProposal = _depositProposals[originChainID][depositID];
+    function voteDepositProposal(
+        uint256 originChainID,
+        address originChainHandlerAddress,
+        uint256 depositID,
+        Vote vote
+    ) public _onlyValidators {
+        DepositProposal storage depositProposal = _depositProposals[originChainID][originChainHandlerAddress][depositID];
 
         require(depositProposal._status != DepositProposalStatus.Inactive, "proposal is not active");
         require(depositProposal._status == DepositProposalStatus.Active, "proposal has been finalized");
@@ -316,17 +192,23 @@ contract Bridge {
         // Todo: Edge case if validator threshold changes?
         if (depositProposal._numYes >= _validatorThreshold) {
             depositProposal._status = DepositProposalStatus.Passed;
-            emit DepositProposalFinalized(originChainID, depositID);
+            emit DepositProposalFinalized(originChainID, originChainHandlerAddress, depositID);
         } else if (_validatorContract.getTotalValidators().sub(depositProposal._numNo) < _validatorThreshold) {
             depositProposal._status = DepositProposalStatus.Denied;
-            emit DepositProposalFinalized(originChainID, depositID);
+            emit DepositProposalFinalized(originChainID, originChainHandlerAddress, depositID);
         }
 
-        emit DepositProposalVote(originChainID, depositID, vote, depositProposal._status);
+        emit DepositProposalVote(originChainID, originChainHandlerAddress, depositID, vote, depositProposal._status);
     }
 
-    function executeDepositProposal(uint originChainID, uint depositID, address destinationChainHandlerAddress, bytes memory data) public {
-        DepositProposal storage depositProposal = _depositProposals[originChainID][depositID];
+    function executeDepositProposal(
+        uint256      originChainID,
+        address      originChainHandlerAddress,
+        uint256      depositID,
+        address      destinationChainHandlerAddress,
+        bytes memory data
+    ) public {
+        DepositProposal storage depositProposal = _depositProposals[originChainID][originChainHandlerAddress][depositID];
 
         require(depositProposal._status != DepositProposalStatus.Inactive, "proposal is not active");
         require(depositProposal._status == DepositProposalStatus.Passed, "proposal was not passed or has already been transferred");
@@ -336,6 +218,7 @@ contract Bridge {
         depositHandler.executeDeposit(data);
 
         depositProposal._status = DepositProposalStatus.Transferred;
+        emit DepositProposalExecuted(originChainID, originChainHandlerAddress, depositID);
     }
 
     function createValidatorThresholdProposal(uint proposedValue) public _onlyValidators {
