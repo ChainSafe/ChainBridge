2ea4ab0ef8fe5d97b821a29c9614e9cca9a0b42b
Gregory Markou <16929357+GregTheGreek@users.noreply.github.com>
2020-03-16 15:45:52 -0400

    Add runtime bytecode to go bindings (#197)
    add runtimebytecode to the script

--- scripts/evm/create_go_bindings.sh
+++ scripts/evm/create_go_bindings.sh
@@ -9,6 +9,7 @@ contract_binding_path="on-chain/evm-contracts/"
 base_path="./build/tmp"
 BIN_DIR="$base_path/bin"
 ABI_DIR="$base_path/abi"
+RUNTIME_DIR="$base_path/runtime"
 GO_DIR="$base_path/go"
 
 # Remove old bin and abi
@@ -18,10 +19,12 @@ mkdir $base_path
 mkdir $ABI_DIR
 mkdir $BIN_DIR
 mkdir $GO_DIR
+mkdir $RUNTIME_DIR
 
 echo "Copying new builds to root..."
 cp -r $contract_binding_path/bindings/abi/* $ABI_DIR
 cp -r $contract_binding_path/bindings/bin/* $BIN_DIR
+cp -r $contract_binding_path/bindings/runtime/* $RUNTIME_DIR
 
 for file in "$BIN_DIR"/*.bin
 do
@@ -34,6 +37,13 @@ do
     
     # Build the go package
     abigen --abi $ABI_DIR/${value}.abi --pkg $value --type $value --bin $BIN_DIR/${value}.bin --out $GO_DIR/$value/$value.go
+    
+    # Capture build temp 
+    bytecode=`cat ./build/tmp/runtime/Bridge.bin`
+    # prefix="var RuntimeBytecode = "
+    # variable=$prefix"$bytecode"
+    variable="var RuntimeBytecode = \"${bytecode}\""
+    echo $variable >> $GO_DIR/$value/$value.go
 done
 
 # Remove old bindings
