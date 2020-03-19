481efe3f8e6ae337eb4d83ed8facce11d8fdff50
David Ansermino <david@chainsafe.io>
2020-03-19 18:30:43 -0400

    Fix build number

--- scripts/centrifuge/run_chain_ci.sh
+++ scripts/centrifuge/run_chain_ci.sh
@@ -4,14 +4,14 @@
 
 S3_URL="https://centchain.nyc3.digitaloceanspaces.com"
 CENT_CHAIN_COMMIT="6d1a3f1641513ae211ad46f80e8cd739eb6a0619"
-CENT_CHAIN_JOB_ID="299873451"
+CENT_CHAIN_BUILD_ID="154078537"
 CENT_CHAIN_BRANCH="david/add-bridge"
 
 set -eux
 
 rm -rf ./centrifuge-chain
 
-wget $S3_URL/$CENT_CHAIN_COMMIT-$CENT_CHAIN_JOB_ID/centrifuge-chain
+wget $S3_URL/$CENT_CHAIN_COMMIT-$CENT_CHAIN_BUILD_ID/centrifuge-chain
 
 chmod a+x ./centrifuge-chain
 
