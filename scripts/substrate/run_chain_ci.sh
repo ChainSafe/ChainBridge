ed42a168179c38e50c751296d3244d8e589447c5
David Ansermino <david@chainsafe.io>
2020-03-27 07:40:36 -0400

    Update ci config

--- scripts/substrate/run_chain_ci.sh
+++ scripts/substrate/run_chain_ci.sh
@@ -3,15 +3,15 @@
 # SPDX-License-Identifier: LGPL-3.0-only
 
 S3_URL="https://centchain.nyc3.digitaloceanspaces.com"
-SUB_COMMIT="4bf5a2b7b3cba16a9c1c0ab93115fd4886502733"
-SUB_BUILD_ID="155080360"
+SUB_COMMIT="4745a59dbe5b24222376ffbad821d0c253a76592"
+SUB_BUILD_ID="155725367"
 SUB_CMD="chainbridge-substrate-chain"
 
 set -eux
 
 rm -rf ./$SUB_CMD
 
-wget $S3_URL/$SUB_CMD/$SUB_COMMIT-$CENT_BUILD_ID/$SUB_CMD
+wget $S3_URL/$SUB_CMD/$SUB_COMMIT-$SUB_BUILD_ID/$SUB_CMD
 
 chmod a+x ./$SUB_CMD
 
