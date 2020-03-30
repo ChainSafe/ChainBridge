5deeb7f070b5d657e4dabe91df7df764b43bda23
steviezhang <stephanie@chainsafe.io>
2020-03-30 12:48:47 -0400

    changed to latest hash

--- scripts/setup_contracts.sh
+++ scripts/setup_contracts.sh
@@ -4,7 +4,7 @@
 
 CONTRACTS_REPO="https://github.com/ChainSafe/chainbridge-solidity"
 CONTRACTS_BRANCH="wyatt/migrate-pr-200"
-CONTRACTS_COMMIT="a4add24d01d3c595a6bfc3c80c300126eb9a7a3d"
+CONTRACTS_COMMIT="2634b786afe01104516df4c6a5a404dd3007aa7b"
 CONTRACTS_DIR="./solidity"
 DEST_DIR="./bindings"
 
