d0c2a0d7a88c23e476f26e555b4166cfe13e8e43
David Ansermino <david@chainsafe.io>
2020-05-29 09:31:31 -0400

    Bump again

--- scripts/setup_contracts.sh
+++ scripts/setup_contracts.sh
@@ -4,7 +4,7 @@
 
 CONTRACTS_REPO="https://github.com/ChainSafe/chainbridge-solidity"
 CONTRACTS_BRANCH="master"
-CONTRACTS_COMMIT="5fbc2bedddaf6e38a77047f53ab0bd40e7824fef"
+CONTRACTS_COMMIT="5382f0c889b4c4e61a1700568ef005af6a5b24e7"
 CONTRACTS_DIR="./solidity"
 DEST_DIR="./bindings"
 
