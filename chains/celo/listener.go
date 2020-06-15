1a8054153642e7ab1a6162789de9fac4d2fa9e89
Alex <alex@chainsafe.io>
2020-06-15 15:59:51 -0400

    adding getTransactionBlockHash and getBlockTransactionsByHash

--- chains/celo/listener.go
+++ chains/celo/listener.go
@@ -4,7 +4,13 @@
 package celo
 
 import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+
 	connection "github.com/ChainSafe/ChainBridge/connections/ethereum"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var _ Connection = &connection.Connection{}
@@ -12,6 +18,7 @@ var _ Connection = &connection.Connection{}
 type Connection interface {
 	Connect() error
 	Close()
+	Client() *ethclient.Client
 }
 
 type listener struct {
@@ -32,4 +39,22 @@ func (l *listener) start() error {
 
 func (l *listener) close() {
 	l.conn.Close()
+}
+
+func (l *listener) getTransactionBlockHash(hash common.Hash) (tx *types.Transaction) {
+	client := l.conn.Client()
+	blockHash, _, err := client.TransactionByHash(context.Background(), hash)
+	if err != nil {
+		fmt.Errorf("unable to get BlockHash: %s", err)
+	}
+	return blockHash
+}
+
+func (l *listener) getBlockTransactionsByHash(hash common.Hash) *types.Block {
+	client := l.conn.Client()
+	block, err := client.BlockByHash(context.Background(), hash)
+	if err != nil {
+		return block
+	}
+
 }
