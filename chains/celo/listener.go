4904595039f915cd4a5d7b42707bcc93264635ba
Alex <alex@chainsafe.io>
2020-06-22 17:01:10 -0400

    Celo listener adding RPC calls #423 (#459)
    * creating celo folder with listener struct and connection interface
    
    * adding listener_test
    
    * fixing up celo tests
    
    * pushing test
    
    * Add subkey to docker build (#455)
    
    Includes subkey in dockerfile to support substrate key signing
    
    * replacing methods that take in listeners with receiver methods and changing up tests
    
    * deleting unneccesary file
    
    * adding getTransactionBlockHash and getBlockTransactionsByHash
    
    * adding celo-dev branch to travis config
    
    * Add release docker push (#460)
    
    * Returns nil on unrecognized event (#458)
    
    * Fix docker push (#461)
    
    * changed up return statements and added more testing
    
    * changed getBlockTransactionByHash to return array of hashes
    
    * creating and submitting a transaction in listener_test
    
    * Revert "Merge branch 'master' into alex/celoListener-2"
    
    This reverts commit ff30ab14ecac514ab912feadfb5e5e11792452a6, reversing
    changes made to d8e876ce90ff194c8d00145e387d9ad80e47c714.
    
    * fixing up
    
    * adding waitflorblocks for cleo
    
    * finishing transactionblockhash
    
    * updating tests
    
    * refactored waitForTx to test file, fixing up tests again
    
    * fixing linter
    
    * linter errors
    
    * addressing feedback
    
    * fixing error
    
    * addressing feedback
    
    Co-authored-by: David Ansermino <david@chainsafe.io>

--- chains/celo/listener.go
+++ chains/celo/listener.go
@@ -4,7 +4,14 @@
 package celo
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	connection "github.com/ChainSafe/ChainBridge/connections/ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var _ Connection = &connection.Connection{}
@@ -12,7 +19,11 @@ var _ Connection = &connection.Connection{}
 type Connection interface {
 	Connect() error
 	Close()
+	Client() *ethclient.Client
+	Opts() *bind.TransactOpts
 }
+
+var ExpectedBlockTime = time.Second
 
 type listener struct {
 	conn Connection
@@ -32,4 +43,29 @@ func (l *listener) start() error {
 
 func (l *listener) close() {
 	l.conn.Close()
+}
+
+func (l *listener) getBlockHashFromTransactionHash(txHash common.Hash) (blockHash common.Hash, err error) {
+
+	receipt, err := l.conn.Client().TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		return txHash, fmt.Errorf("unable to get BlockHash: %s", err)
+	}
+	return receipt.BlockHash, nil
+}
+
+func (l *listener) getTransactionsFromBlockHash(blockHash common.Hash) (txHashes []common.Hash, txRoot common.Hash, err error) {
+	block, err := l.conn.Client().BlockByHash(context.Background(), blockHash)
+	if err != nil {
+		return nil, common.Hash{}, fmt.Errorf("unable to get BlockHash: %s", err)
+	}
+
+	var transactionHashes []common.Hash
+
+	transactions := block.Transactions()
+	for _, transaction := range transactions {
+		transactionHashes = append(transactionHashes, transaction.Hash())
+	}
+
+	return transactionHashes, block.Root(), nil
 }
--- chains/celo/listener_test.go
+++ chains/celo/listener_test.go
@@ -4,23 +4,83 @@
 package celo
 
 import (
+	"context"
+	"fmt"
 	"math/big"
 	"testing"
+	"time"
 
 	connection "github.com/ChainSafe/ChainBridge/connections/ethereum"
 	"github.com/ChainSafe/ChainBridge/keystore"
 	ethutils "github.com/ChainSafe/ChainBridge/shared/ethereum"
 	"github.com/ChainSafe/log15"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 var TestEndpoint = "ws://localhost:8545"
 var AliceKp = keystore.TestKeyRing.EthereumKeys[keystore.AliceKey]
 var GasLimit = big.NewInt(ethutils.DefaultGasLimit)
 var GasPrice = big.NewInt(ethutils.DefaultGasPrice)
+var ZeroAddress = common.HexToAddress("0x0000000000000000000000000000000000000000")
 
-func TestListener_start_stop(t *testing.T) {
+func createTestListener(t *testing.T) *listener {
 	conn := connection.NewConnection(TestEndpoint, false, AliceKp, log15.Root(), GasLimit, GasPrice)
 	l := NewListener(conn)
+
+	return l
+}
+
+// creating and sending a new transaction
+func newTransaction(t *testing.T, l *listener) common.Hash {
+
+	// Creating a new transaction
+	nonce := l.conn.Opts().Nonce
+	tx := types.NewTransaction(nonce.Uint64(), ZeroAddress, big.NewInt(0), ethutils.DefaultGasLimit, GasPrice, nil)
+
+	chainId, err := l.conn.Client().NetworkID(context.Background())
+	signer := types.NewEIP155Signer(chainId)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign the transaction
+	signedTx, err := types.SignTx(tx, signer, AliceKp.PrivateKey()) //SignTx(tx *Transaction, s Signer, prv *ecdsa.PrivateKey) (*Transaction, error)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Send the transaction for execution
+	err = l.conn.Client().SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return signedTx.Hash()
+}
+
+// WaitForTx will query the chain at ExpectedBlockTime intervals, until a receipt is returned.
+// Returns an error if the tx failed.
+func waitForTx(client *ethclient.Client, hash common.Hash) (*types.Receipt, error) {
+	retry := 10
+	for retry > 0 {
+		receipt, err := client.TransactionReceipt(context.Background(), hash)
+		if err != nil {
+			retry--
+			time.Sleep(ExpectedBlockTime)
+			continue
+		}
+
+		if receipt.Status != 1 {
+			return nil, fmt.Errorf("transaction failed on chain")
+		}
+		return receipt, nil
+	}
+	return nil, fmt.Errorf("transaction after retries failed")
+}
+
+func TestListener_start_stop(t *testing.T) {
+	l := createTestListener(t)
 
 	err := l.start()
 	if err != nil {
@@ -29,4 +89,63 @@ func TestListener_start_stop(t *testing.T) {
 
 	// Initiate shutdown
 	l.close()
+}
+
+// Testing transaction Block hash
+func TestListener_BlockHashFromTransactionHash(t *testing.T) {
+
+	l := createTestListener(t)
+	err := l.start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and submit a new transaction and return the signed transaction hash
+	txHash := newTransaction(t, l)
+
+	receipt, err := waitForTx(l.conn.Client(), txHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	blockHash, err := l.getBlockHashFromTransactionHash(txHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Confirm that the receipt blockhash and the block's blockhash are the same
+	if blockHash != receipt.BlockHash {
+		t.Fatalf("block hash are not equal, expected: %x, %x", receipt.BlockHash, blockHash)
+	}
+}
+
+func TestListener_TransactionsFromBlockHash(t *testing.T) {
+	l := createTestListener(t)
+	err := l.start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create and submit a new transaction
+	txHash := newTransaction(t, l)
+
+	// Get receipt from hash
+	receipt, err := waitForTx(l.conn.Client(), txHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get txHashes and txroot from blockHash
+	txs, _, err := l.getTransactionsFromBlockHash(receipt.BlockHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(txs) != 1 {
+		t.Fatalf("transaction hashes should have a length of one, %x", txs)
+	}
+
+	if txs[0] != txHash {
+		t.Fatalf("hash and transactions should be the same: hash, %x txs, %x", txHash, txs)
+	}
 }
