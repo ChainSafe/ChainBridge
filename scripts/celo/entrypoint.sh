dd4772cbab260ec2999667b74aff9faf9dc76bec
luu-alex <alex@chainsafe.io>
2020-07-13 09:54:48 -0400

    removing uneccesary import

--- scripts/celo/entrypoint.sh
+++ scripts/celo/entrypoint.sh
@@ -9,11 +9,10 @@ geth init /root/genesis.json
 
 rm -rf /root/.celo/keystore
 cp -r /root/keystore /root/.celo/
-geth account import /root/key.txt
 
 exec geth \
   --nodiscover \
-  --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485","0xf4314cb9046bece6aa54bb9533155434d0c76909" \
+  --unlock "0xf4314cb9046bece6aa54bb9533155434d0c76909","0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" \
   --password /root/password.txt \
   --ws \
   --wsport 8545 \
