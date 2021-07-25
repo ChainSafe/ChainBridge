167675bce1dae10c5a573230472f3093db877733
Takuma Katsumata <tk.30.r.h@gmail.com>
2021-07-25 12:51:26 +0900

    feat: add minGasPrice field to Connection struct

--- connections/ethereum/connection.go
+++ connections/ethereum/connection.go
@@ -29,6 +29,7 @@ type Connection struct {
 	kp            *secp256k1.Keypair
 	gasLimit      *big.Int
 	maxGasPrice   *big.Int
+	minGasPrice   *big.Int
 	gasMultiplier *big.Float
 	egsApiKey     string
 	egsSpeed      string
@@ -43,13 +44,14 @@ type Connection struct {
 }
 
 // NewConnection returns an uninitialized connection, must call Connection.Connect() before using.
-func NewConnection(endpoint string, http bool, kp *secp256k1.Keypair, log log15.Logger, gasLimit, gasPrice *big.Int, gasMultiplier *big.Float, gsnApiKey, gsnSpeed string) *Connection {
+func NewConnection(endpoint string, http bool, kp *secp256k1.Keypair, log log15.Logger, gasLimit, maxGasPrice, minGasPrice *big.Int, gasMultiplier *big.Float, gsnApiKey, gsnSpeed string) *Connection {
 	return &Connection{
 		endpoint:      endpoint,
 		http:          http,
 		kp:            kp,
 		gasLimit:      gasLimit,
-		maxGasPrice:   gasPrice,
+		maxGasPrice:   maxGasPrice,
+		minGasPrice:   minGasPrice,
 		gasMultiplier: gasMultiplier,
 		egsApiKey:     gsnApiKey,
 		egsSpeed:      gsnSpeed,
